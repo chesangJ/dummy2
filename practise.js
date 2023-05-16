988e3a385aa842b03ec6c4c126c43566715cc2bf
chesangJ <mitingijoychesang@gmail.com>
2023-05-16 07:34:07 +0300

    dummy

---
+++ practise.js
@@ -0,0 +1,5 @@
+//Write a function that takes two strings as input and returns the
+// index of the first occurrence of the second string in the first 
+//string. If the second string does not occur in the first string, the function
+// should return -1.
+ 
